aa804066dd46030a81484c66c8069a44bf783ff4
Dennis Aldana <dennis.aldana@gmail.com>
2024-09-10 09:23:44 -0600

    fix: add polyfill for the randomUUID in https

--- .ladle/components.jsx
+++ .ladle/components.jsx
@@ -8,6 +8,14 @@ export const Provider = ({ children, globalState }) => {
     } else {
       document.documentElement.classList.remove("dark")
     }
+    if (!window.crypto.randomUUID) {
+  	window.crypto.randomUUID = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+        return v.toString(16)
+      })
+    }
+}
   }, [globalState.theme])
   return children
 }
